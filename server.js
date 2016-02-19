86f434258a59bf21cac9168fc461a7b61ae3ed11
natac13 <sean.campbell13@gmail.com>
2016-02-19 15:16:07 -0500

    (Completed):Project is finished
    Need to upload to heroku from here.

--- server.js
+++ server.js
@@ -3,26 +3,36 @@ const moment = require('moment');
 const app = express();
 
 app.get('/', function(req, res) {
-  res.send('Input date in url query');
-})
+  res.send('You can input a date string into the url and get back an json object with the unix date stamp and the natural date for properties');
+});
+
+
 app.get('/:date', function(req, res) {
-  console.log(req.params);
   const dateString = req.params.date;
 
-  const regex = /\w+\s\d{1,2},\s\d{4}/i;
+  const dateObj = new Date(dateString);
+  const dateMoment = moment.utc(dateObj); // moment date object
+  const dateUnix = moment.unix(dateString); // moment date unix object
 
-  if (regex.test(dateString)) {
-    const dateObj = new Date(dateString);
-    const dateMoment = moment(dateObj);
+  if (dateMoment.isValid()) { // check if valid date with string
     const dateToDisplay = {
       unix: dateMoment.unix(),
       natural: dateMoment.format('MMMM Do, YYYY')
       }
     res.json(dateToDisplay);
 
+  } else if (dateUnix.isValid()) { // checks if valid date for unix number
+    const dateToDisplay2 = {
+      unix: dateUnix.unix(),
+      natural: dateUnix.format('MMMM Do, YYYY')
+      }
+    res.json(dateToDisplay2);
   } else {
+    // redirect on any wrong inputs into the url query
     res.redirect('/');
   }
+
+
 
 });
 
